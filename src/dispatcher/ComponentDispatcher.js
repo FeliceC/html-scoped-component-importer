271cb2755d4169e9efd706383ade10420e42dd4d
Felice Caricati <felice.caricati@openmindonline.it>
2021-02-12 17:13:13 +0100

    uuid as dependecy and fix uuid import

--- src/dispatcher/ComponentDispatcher.js
+++ src/dispatcher/ComponentDispatcher.js
@@ -1,25 +1,25 @@
-import uuid from 'uuid/v1';
+import { uuid as v1 } from "uuid";
 
 const APP_COMPONENTS = new Map();
 
 export default class ComponentsDispatcher {
-  get APP_COMPONENTS () {
+  get APP_COMPONENTS() {
     return APP_COMPONENTS;
   }
 
   set Selector(selector) {
-    this.componentSelector = selector || '[data-component]';
+    this.componentSelector = selector || "[data-component]";
   }
 
   get compAttribute() {
     return this.componentSelector
-      .replace('data-', '')
-      .replace('[', '')
-      .replace(']', '');
+      .replace("data-", "")
+      .replace("[", "")
+      .replace("]", "");
   }
 
   constructor() {
-    this.componentSelector = '[data-component]';
+    this.componentSelector = "[data-component]";
     this.domComponents = null;
     this.componentsNames = null;
     this.compsObj = null;
@@ -30,12 +30,13 @@ export default class ComponentsDispatcher {
     let domComponents = [];
     const target = parent || document.body;
     try {
-      domComponents = [
-        ...target.querySelectorAll(this.componentSelector)
-      ].filter(el => !el.UUID).map((el) => {
-        el.UUID = uuid();
-        return el;
-      }).reverse();
+      domComponents = [...target.querySelectorAll(this.componentSelector)]
+        .filter((el) => !el.UUID)
+        .map((el) => {
+          el.UUID = uuid();
+          return el;
+        })
+        .reverse();
     } catch (e) {
       console.error(e);
     }
@@ -46,8 +47,8 @@ export default class ComponentsDispatcher {
     let compNames;
     try {
       const names = components
-        .filter(el => el.dataset.component !== '')
-        .map(comp => comp.dataset.component.split(','));
+        .filter((el) => el.dataset.component !== "")
+        .map((comp) => comp.dataset.component.split(","));
       compNames = [...new Set(names.flat())];
     } catch (e) {
       console.error(e);
@@ -61,34 +62,40 @@ export default class ComponentsDispatcher {
     const config = { attributes: false, childList: true, subtree: true };
     const observer = new MutationObserver((mutationsList, observer) => {
       for (let mutation of mutationsList) {
-        if (mutation.type === 'childList') {
+        if (mutation.type === "childList") {
           if (mutation.target && [...mutation.addedNodes].length) {
-            [...mutation.addedNodes].filter(el => typeof el.querySelectorAll === 'function').forEach((addedNode) => {
-              if (this.async) {
-                this.importAsyncComponents(addedNode);
-              } else {
-                this.importComponents(this.compsObj, addedNode);
-              }
-            });
+            [...mutation.addedNodes]
+              .filter((el) => typeof el.querySelectorAll === "function")
+              .forEach((addedNode) => {
+                if (this.async) {
+                  this.importAsyncComponents(addedNode);
+                } else {
+                  this.importComponents(this.compsObj, addedNode);
+                }
+              });
           }
 
           if (mutation.target && [...mutation.removedNodes].length) {
-            [...mutation.removedNodes].filter(el => typeof el.querySelectorAll === 'function').forEach((removedNodes) => {
-              const components = removedNodes.querySelectorAll(this.componentSelector);
-              [...components].forEach((comp) => {
-                const COMP_ID = comp.UUID;
-                if (COMP_ID && APP_COMPONENTS.has(COMP_ID)) {
-                  const removedComp = APP_COMPONENTS.get(COMP_ID);
-                  APP_COMPONENTS.delete(COMP_ID);
-                  removedComp._destroy();
-                }
+            [...mutation.removedNodes]
+              .filter((el) => typeof el.querySelectorAll === "function")
+              .forEach((removedNodes) => {
+                const components = removedNodes.querySelectorAll(
+                  this.componentSelector
+                );
+                [...components].forEach((comp) => {
+                  const COMP_ID = comp.UUID;
+                  if (COMP_ID && APP_COMPONENTS.has(COMP_ID)) {
+                    const removedComp = APP_COMPONENTS.get(COMP_ID);
+                    APP_COMPONENTS.delete(COMP_ID);
+                    removedComp._destroy();
+                  }
+                });
               });
-            });
           }
           // const newNodes = mutation.addedNodes;
-        } else if (mutation.type === 'attributes') {
+        } else if (mutation.type === "attributes") {
           console.log(
-            'The ' + mutation.attributeName + ' attribute was modified.'
+            "The " + mutation.attributeName + " attribute was modified."
           );
         }
       }
@@ -102,7 +109,9 @@ export default class ComponentsDispatcher {
     try {
       const components = this.getDomComponents(target);
       for (const compName of Object.keys(comps)) {
-        const el = components.filter(elelemt => elelemt.dataset[this.compAttribute].split(',').includes(compName));
+        const el = components.filter((elelemt) =>
+          elelemt.dataset[this.compAttribute].split(",").includes(compName)
+        );
         if (el.length > 0) {
           el.forEach((compEl) => {
             try {
@@ -131,27 +140,37 @@ export default class ComponentsDispatcher {
       if (components.length === 0) return;
 
       names.forEach((compName) => {
-        components.filter(el => el.dataset[this.compAttribute].split(',').includes(compName.trim())).forEach(async (el) => {
-          try {
-            let componentModule = this.importedComponents.get(compName);
-            if (!componentModule) {
-              const res = await import(/* webpackChunkName: `component-[request]` */ `COMPONENTS/${compName}.js`);
-              const DynamicComponent = res.default;
-              this.importedComponents.set(compName, DynamicComponent);
-              const comp = new DynamicComponent(el);
-              APP_COMPONENTS.set(el.UUID, comp);
-            } else {
-              const comp = new componentModule(el);
-              APP_COMPONENTS.set(el.UUID, comp);
+        components
+          .filter((el) =>
+            el.dataset[this.compAttribute].split(",").includes(compName.trim())
+          )
+          .forEach(async (el) => {
+            try {
+              let componentModule = this.importedComponents.get(compName);
+              if (!componentModule) {
+                const res = await import(
+                  /* webpackChunkName: `component-[request]` */ `COMPONENTS/${compName}.js`
+                );
+                const DynamicComponent = res.default;
+                this.importedComponents.set(compName, DynamicComponent);
+                const comp = new DynamicComponent(el);
+                APP_COMPONENTS.set(el.UUID, comp);
+              } else {
+                const comp = new componentModule(el);
+                APP_COMPONENTS.set(el.UUID, comp);
+              }
+            } catch (e) {
+              console.error(
+                `[${compName}] initialization error on el =>`,
+                el,
+                "with error =>",
+                e
+              );
             }
-          } catch (e) {
-            console.error(`[${compName}] initialization error on el =>`, el, 'with error =>', e);
-          }
-        });
+          });
       });
-
     } catch (e) {
-      console.error('async importAsyncComponents(target) => ', e);
+      console.error("async importAsyncComponents(target) => ", e);
       this.importEnded = false;
     }
     this.importEnded = true;
